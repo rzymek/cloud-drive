5fdd204e83356c850308a4c67799887736b85931
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2018-03-20 18:10:43 +0100

    using request npm package, as suggested at https://cloud.google.com/functions/docs/concepts/exec#network

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-const google = require("googleapis");
-const VER = "v6";
+const request = require('request');
 
-exports.drive = (req, res) => {
-    console.log(VER);
-    console.log(process.env);
-    const service = google.drive('v3');
-    service.files.list({}, (err, response) => {
-        if (err) {
-            console.error(err);
-            console.error(err.request._headers);
-            res.end(JSON.stringify(err.errors, null, ' '));
-            return;
-        }
-        const files = response.files;
-        console.log("files:", files);
-        if (files.length === 0) {
-            res.end('No files found.');
-        }
-        else {
-            res.end(files.map(file => file.name).join("\n"));
-        }
-    });
+exports.function = (req, res) => {
+  request('https://www.googleapis.com/drive/v3/files', (err, response, body) => {
+    if (err) {
+      console.error(err);
+      console.error(err.request._headers);
+      res.end(JSON.stringify(err.errors, null, ' '));
+      return;
+    }
+    res.end(body);
+  });
 }
