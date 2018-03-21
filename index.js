8eec190c70f3f595b2f9c02d746d7fd52e579a9a
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2018-03-21 09:30:16 +0100

    using google-auth-library, does not work still

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const google = require("googleapis");
-const VER = "v6";
+const { auth } = require('google-auth-library');
+const VER = "v7";
 
 exports.drive = (req, res) => {
-    console.log(VER);
-    console.log(process.env);
-    const service = google.drive('v3');
-    service.files.list({}, (err, response) => {
-        if (err) {
-            console.error(err);
-            console.error(err.request._headers);
-            res.end(JSON.stringify(err.errors, null, ' '));
-            return;
-        }
-        const files = response.files;
-        console.log("files:", files);
-        if (files.length === 0) {
-            res.end('No files found.');
-        }
-        else {
-            res.end(files.map(file => file.name).join("\n"));
-        }
+  console.log(VER);
+  return auth.getApplicationDefault().then(res => {
+    let client = res.credential;
+
+    // The createScopedRequired method returns true when running on GAE or a local developer
+    // machine. In that case, the desired scopes must be passed in manually. When the code is
+    // running in GCE or a Managed VM, the scopes are pulled from the GCE metadata server.
+    // See https://cloud.google.com/compute/docs/authentication for more information.
+    if (client.createScopedRequired && client.createScopedRequired()) {
+      // Scopes can be specified either as an array or as a single, space-delimited string.
+      const scopes = ['https://www.googleapis.com/auth/cloud-platform'];
+      client = client.createScoped(scopes);
+    }
+    return client.request({
+      url: 'https://www.googleapis.com/drive/v3/files'
     });
+  }).then(res => {
+    console.log({data:res.data});
+    return res.end(res.data);
+  }).catch(err => res.end(err.message))
 }
